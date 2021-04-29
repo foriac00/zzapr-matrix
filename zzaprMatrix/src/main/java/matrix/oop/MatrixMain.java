4ead4e28e0b3ddd759309dd082945f6dd8259a20
Michael Bazant <bazantm@gmail.com>
2021-04-29 17:42:28 +0200

    Paths, Files - how to work with folders and files

--- zzaprMatrix/src/main/java/matrix/oop/MatrixMain.java
+++ zzaprMatrix/src/main/java/matrix/oop/MatrixMain.java
@@ -5,6 +5,7 @@
  */
 package matrix.oop;
 
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -18,49 +19,49 @@ import static matrix.practice.MatrixPractice.SwapDiagonals;
  */
 public class MatrixMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
         //comment from bazantm
         Scanner enter = new Scanner(System.in);
 
         int rowA, colA, rowB, colB;
         Random r = new Random();
 
-        System.out.println("Establish the number of rows of the matrix A: ");
-        rowA = enter.nextInt();
+//        System.out.println("Establish the number of rows of the matrix A: ");
+//        rowA = enter.nextInt();
+//
+//        System.out.println("Establish the number of columns of the matrix A: ");
+//        colA = enter.nextInt();
+//
+//        System.out.println("Establish the number of rows of the matrix B: ");
+//        rowB = enter.nextInt();
+//
+//        System.out.println("Establish the number of columns of the matrix B: ");
+//        colB = enter.nextInt();
 
-        System.out.println("Establish the number of columns of the matrix A: ");
-        colA = enter.nextInt();
-
-        System.out.println("Establish the number of rows of the matrix B: ");
-        rowB = enter.nextInt();
-
-        System.out.println("Establish the number of columns of the matrix B: ");
-        colB = enter.nextInt();
-
-        Matrix matrixA = new Matrix("Matrix A", rowA, colA);
+        Matrix matrixA = new Matrix("Matrix A", 6, 2);
         matrixA.generateRandomElements(-10, 10);
         matrixA.getPrincipalDiagonal();
         System.out.println(matrixA);
         //System.out.println("Principal Diagonal: " + Arrays.toString(matrixA.getPrincipalDiagonal()));
         TextFiles.saveMatrix(matrixA, "matrixA-before.txt");
 
-        Matrix matrixB = new Matrix("Matrix B", rowB, colB);
-        matrixB.generateRandomElements(-10, 10);
-        matrixB.getSecondaryDiagonal();
-        System.out.println(matrixB);
-        //System.out.println("Secondary Diagonal: " + Arrays.toString(matrixB.getSecondaryDiagonal()));
-        TextFiles.saveMatrix(matrixB, "matrixB-before.txt");
+//        Matrix matrixB = new Matrix("Matrix B", rowB, colB);
+//        matrixB.generateRandomElements(-10, 10);
+//        matrixB.getSecondaryDiagonal();
+//        System.out.println(matrixB);
+//        //System.out.println("Secondary Diagonal: " + Arrays.toString(matrixB.getSecondaryDiagonal()));
+//        TextFiles.saveMatrix(matrixB, "matrixB-before.txt");
 
 //        try{
 //            BufferedWriter bw = new BufferedWriter(new FileWriter(new File("fileOutput.txt")));
 //            
 //        }
-        matrixA.swapDiagonals(matrixB);
-        System.out.println("Swapped " + matrixA);
-        System.out.println("Swapped " + matrixB);
-
-        TextFiles.saveMatrix(matrixA, "matrixA-after.txt");
-        TextFiles.saveMatrix(matrixB, "matrixB-after.txt");
+//        matrixA.swapDiagonals(matrixB);
+//        System.out.println("Swapped " + matrixA);
+//        System.out.println("Swapped " + matrixB);
+//
+//        TextFiles.saveMatrix(matrixA, "matrixA-after.txt");
+//        TextFiles.saveMatrix(matrixB, "matrixB-after.txt");
 
     }
 }
--- zzaprMatrix/src/main/java/matrix/oop/files/TextFiles.java
+++ zzaprMatrix/src/main/java/matrix/oop/files/TextFiles.java
@@ -2,18 +2,33 @@ package matrix.oop.files;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import matrix.oop.Matrix;
 
 public class TextFiles {
     
-    public static void saveMatrix(Matrix m, String fileName) {
-        Path path = Paths.get(fileName);
+    public static void saveMatrix(Matrix m, String fileName) throws URISyntaxException {
+               
+        Path dataDir = Paths.get("data");
         
-        try (BufferedWriter bw = Files.newBufferedWriter(path)) {
-            
+        if (!Files.exists(dataDir)) {
+            try {
+                Files.createDirectories(dataDir);
+            } catch (IOException ex) {
+                Logger.getLogger(TextFiles.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        Path file = dataDir.resolve(fileName);
+        
+                
+        try (BufferedWriter bw = Files.newBufferedWriter(file)) {
+            bw.write("aaa, bbb, ccc");
         } catch (IOException ex) {
             System.err.println("IO exception");
         }
