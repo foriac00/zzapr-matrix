8f9bcaa377ffbbda43f4e313528f3634dc844b77
foriac00 <foriac00@estudiantes.unileon.es>
2021-05-12 19:23:52 +0200

    Changes in java/matrix/oop/gui/MatrixMainFX.java

--- zzaprMatrix/src/main/java/matrix/oop/gui/MatrixMainFX.java
+++ zzaprMatrix/src/main/java/matrix/oop/gui/MatrixMainFX.java
@@ -3,12 +3,16 @@ package matrix.oop.gui;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.event.Event;
+import matrix.oop.Matrix;
+import matrix.oop.MatrixMain;
 
 /**
  *
@@ -20,20 +24,32 @@ public class MatrixMainFX extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        
 
-        primaryStage.setTitle("Matrix");
-        BorderPane root = new BorderPane();
-        
-        textArea = new TextArea();
-        
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    Group root = new Group();
+		
+		Scene scene = new Scene( root, 300, 300 );
+		
+		primaryStage.setTitle("Matrix");
+		
+		primaryStage.setScene( scene );
+		
+		
+		Button boton = new Button("Show");
+		boton.setDefaultButton(true);
+		
+		boton.setPrefSize(100, 50);
+		
+		boton.setLayoutX(105);
+		boton.setLayoutY(110);
+                
+		
+		root.getChildren().add(boton);
+		
+		//Para mostrar la visible, semejante al setVisible(true)
+		primaryStage.show();		
+      
     }
-
     /**
      * @param args the command line arguments
      */
